ba1fd920e200c8407c7b2226692e8d8edf3f3195
Vinicius Matheus <viniciusmattheus@outlook.com>
2022-03-14 22:29:27 -0300

    Aula 4 Operadores

---
+++ 4Operadores/script4_01.js
@@ -0,0 +1,12 @@
+/* 
+Definição das variáveis.
+Variáveis recebendo os valores.
+Operação Artimética de multiplicação(*) e soma(+) na variável x.
+Escrevendo no documento o Resultado das operações e concatenação.
+*/
+
+var x, y, z;
+y=3;
+z=5;
+x=2*y+z;
+document.write("x = "+x)
